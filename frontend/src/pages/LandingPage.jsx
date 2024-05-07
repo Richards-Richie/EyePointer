cb9e00901b604d8d8ba942679397d7aad4db06af
Richards-Richie <danderichards360@gmail.com>
2024-05-07 16:52:14 +0530

    frontend and backeend

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+import Header from './Header';
+function LandingPage(){
+    return(
+        <div>
+        <Header/>
+            <h1>LandingPage</h1>
+        </div>
+    )
+}
+export default LandingPage;
